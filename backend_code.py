9d5e9c9c5a740ca7ac9d5470a6e09c8e8169c6bd
KonradMnich <konrad.mnich@gmail.com>
2021-02-27 14:20:32 +0100

    Add first draft of the web app

---
+++ backend_code.py
@@ -0,0 +1,90 @@
+from datetime import datetime, date
+import pandas as pd
+import pathlib
+import seaborn as sns
+
+
+def asses_temperature(temperature):
+    """"Judge results of temperature
+    value received from the form
+    and return an appropriate message
+
+    Parameters
+    ----------
+    temperature : str
+        String convertible to float
+
+    Returns
+    -------
+    message : str
+        Message informing if the
+        result was correct.
+    temperature : float or str
+        Either converted or not.
+    """
+    try:
+        # replace decimal separator if needed
+        temperature = temperature.replace(",", ".")
+        temperature = float(temperature)
+        if temperature > 40:
+            message = f"{temperature} °C to trochę za gorąco. " \
+                      f"Powtórzyć pomiar?"
+        elif temperature < 35:
+            message = f"Powiało chłodem (wynik: {temperature} °C). " \
+                      f"Powtórzyć pomiar?"
+        else:
+            message = f"Zmierzono {temperature} °C, pomiar wykonany poprawnie."
+    except ValueError:
+        message = "Temperatura wprowadzona niepoprawnie, spróbuj jeszcze raz."
+
+    return message, temperature
+
+
+def save_temperature(temperature, path="temperatures.csv"):
+    """Save temperature with
+    a timestamp to file.
+    """
+    timestamp = date.today().isoformat()
+    path = pathlib.Path(__file__).parent / path
+    if not path.is_file():
+        with open(path, "w") as f:
+            f.write("date,temperature\n")
+            f.write(f"{timestamp},{temperature}\n")
+    else:
+        data = pd.read_csv(path, squeeze=True, index_col=0)
+        data[timestamp] = temperature
+        data.to_csv(path)
+
+
+def create_plot(path="temperatures.csv"):
+    #remove old file
+    image_folder = pathlib.Path(__file__).parent / "static"
+    to_delete = image_folder.glob("temperatures*")
+    for image in to_delete:
+        image.unlink()
+
+    # read data
+    data = pd.read_csv(path)
+    # set plot style
+    sns.set_theme()
+
+    # generate plot
+    my_plot = data.plot(
+        x="date",
+        y="temperature",
+        xlabel="Data",
+        ylabel="Temperatura [C]",
+        title="Pomiary temperatury",
+        style="o--",
+        legend=False
+    )
+    my_fig = my_plot.get_figure()
+    # timestamp generated to prevent browsers from caching
+    image_path = f"static/temperatures_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.svg"
+    my_fig.savefig(image_path, format="svg")
+    return image_path
+
+
+if __name__ =="__main__":
+    #save_temperature(36.8)
+    create_plot()
---
+++ main.py
@@ -0,0 +1,39 @@
+import flask
+import backend_code
+
+
+app = flask.Flask(__name__)
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return flask.render_template("home.html")
+
+
+@app.route("/measurement")
+def measurement():
+    return flask.render_template("measurement.html")
+
+
+@app.route("/summary", methods=["post", "get"])
+def summary():
+    # get value from the form
+    temperature = flask.request.form.get("temp")
+    # adjust the message
+    message, temperature = backend_code.asses_temperature(temperature)
+
+    # save results if appropriate
+    if isinstance(temperature, (float, int)):
+        backend_code.save_temperature(temperature)
+
+    return flask.render_template("summary.html", message=message)
+
+
+@app.route("/visualisation")
+def visualisation():
+    image_path = backend_code.create_plot()
+    return flask.render_template("visualisation.html", image=image_path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
